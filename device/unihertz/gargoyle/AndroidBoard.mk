8f10145dff68cc75287c856ed28f4d4acc3d18c4
rumplestilzken <rumplestilzken@gmail.com>
2023-04-05 05:06:32 -0400

    Removed device

--- device/unihertz/gargoyle/AndroidBoard.mk
+++
@@ -1,5 +0,0 @@
-LOCAL_PATH := $(call my-dir)
-
-ifneq ($(filter gargoyle,$(TARGET_DEVICE)),)
-include $(call all-makefiles-under,$(LOCAL_PATH))
-endif
--- device/unihertz/gargoyle/AndroidProducts.mk
+++
@@ -1,7 +0,0 @@
-PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/omni_gargoyle.mk
-
-COMMON_LUNCH_CHOICES := \
-    omni_gargoyle-user \
-    omni_gargoyle-userdebug \
-    omni_gargoyle-eng
